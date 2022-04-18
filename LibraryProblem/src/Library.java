8a6e45ca14a7cda894f5fec90e748030a82bec93
Damian Coventry <59074625+DamianCoventry@users.noreply.github.com>
2022-04-18 19:48:23 +1200

    Renamed a few items. Updated timeouts.

--- LibraryProblem/src/Library.java
+++ LibraryProblem/src/Library.java
@@ -1,28 +1,28 @@
 public class Library {
     public static final int NUM_BOOKS = 20;
 
-    private final boolean[] _booksBorrowed;
+    private final boolean[] _booksAvailability;
 
     Library() {
-        _booksBorrowed = new boolean[NUM_BOOKS];
+        _booksAvailability = new boolean[NUM_BOOKS];
         for (int i = 0; i <  NUM_BOOKS; ++i) {
-            _booksBorrowed[i] = false;
+            _booksAvailability[i] = false;
         }
     }
 
     public synchronized boolean requestBook(int who, int book) {
-        if (book < 0 || book >= NUM_BOOKS || _booksBorrowed[book]) {
+        if (book < 0 || book >= NUM_BOOKS || _booksAvailability[book]) {
             return false;
         }
         System.out.println("Library gives book " + book + " to thread " + who);
-        _booksBorrowed[book] = true;
+        _booksAvailability[book] = true;
         return true;
     }
 
     public synchronized void returnBook(int who, int book) {
         if (book >= 0 && book < NUM_BOOKS) {
             System.out.println("Library receives returned book " + book + " from thread " + who);
-            _booksBorrowed[book] = false;
+            _booksAvailability[book] = false;
         }
     }
 }
--- LibraryProblem/src/Reader.java
+++ LibraryProblem/src/Reader.java
@@ -5,8 +5,8 @@ public class Reader extends Thread {
     private static final int NUM_BOOKS = 3;
     private final static int MIN_WAIT_TIME = 500;
     private final static int MAX_WAIT_TIME = 1000;
-    private final static int MIN_READ_TIME = 5000;
-    private final static int MAX_READ_TIME = 15000;
+    private final static int MIN_READ_TIME = 1500;
+    private final static int MAX_READ_TIME = 5000;
 
     private final int _id;
     private final Library _library;
@@ -25,9 +25,9 @@ public class Reader extends Thread {
     public void run() {
         try {
             for (int i = 0; i < NUM_BOOKS; ++i) {
-                int book = chooseRandomBook();
+                int book = chooseBook();
                 withdrawBook(book);
-                enthusiasticallyReadBook(book);
+                readBook(book);
                 _library.returnBook(_id, book);       // <-- synchronised
             }
         }
@@ -40,7 +40,7 @@ public class Reader extends Thread {
         }
     }
 
-    private int chooseRandomBook() {
+    private int chooseBook() {
         return _random.nextInt(Library.NUM_BOOKS);
     }
 
@@ -55,7 +55,7 @@ public class Reader extends Thread {
         Thread.sleep(randomWaitTime(MIN_WAIT_TIME, MAX_WAIT_TIME));
     }
 
-    private void enthusiasticallyReadBook(int book) throws InterruptedException {
+    private void readBook(int book) throws InterruptedException {
         System.out.println("Thread " + _id + " is reading book " + book);
         Thread.sleep(randomWaitTime(MIN_READ_TIME, MAX_READ_TIME));
     }
